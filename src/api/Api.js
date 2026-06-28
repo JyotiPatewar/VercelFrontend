b24e8f4cbacc194979aa975204254368ce5a99c2
Jyoti Patewar <jyotipatewar2004@gmail.com>
2026-06-28 23:11:21 +0530

    Fix Netlify routing issue

---
+++ src/api/Api.js
@@ -0,0 +1,52 @@
+
+export default {
+  create_user: "https://manit-clean-track-1.onrender.com/api/user/create-user",
+  send_otp: "https://manit-clean-track-1.onrender.com/api/auth/send-otp",
+  verify_otp: "https://manit-clean-track-1.onrender.com/api/auth/verify-email-otp",
+  get_All_User: "https://manit-clean-track-1.onrender.com/api/user/all-users",
+  create_Emg_Req: "https://manit-clean-track-1.onrender.com/api/emergency/create",
+
+
+
+get_All_Emg_Req:
+"https://manit-clean-track-1.onrender.com/api/emergency/getAllEmgReq",
+
+update_Emg_Order:
+"https://manit-clean-track-1.onrender.com/api/emergency/schedule-req",
+
+get_All_Locations:
+"https://manit-clean-track-1.onrender.com/api/location/all",
+
+get_My_Emg_Req:
+"https://manit-clean-track-1.onrender.com/api/emergency/my-requests",
+
+
+get_Todays_Task:
+"https://manit-clean-track-1.onrender.com/api/driver/today-task",
+
+arrived_At_Task:"https://manit-clean-track-1.onrender.com/api/driver/arrived",
+completed_Task:"https://manit-clean-track-1.onrender.com/api/driver/completed",
+
+// completed_Task: "https://manit-clean-track-1.onrender.com/api/driver/completed",
+
+
+create_Location:"https://manit-clean-track-1.onrender.com/api/location/create",
+
+
+assign_Zone:
+"https://manit-clean-track-1.onrender.com/api/supervisor/assign-zone",
+
+get_Supervisor_Locations:
+"https://manit-clean-track-1.onrender.com/api/supervisor/getsupervisor-location",
+
+get_Locations_By_Zone:
+"https://manit-clean-track-1.onrender.com/api/location/get-location-by-zone",
+
+
+
+delete_User:
+"https://manit-clean-track-1.onrender.com/api/user/delete-user",
+};
+
+
+
